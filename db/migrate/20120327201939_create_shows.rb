1b8585ed16a0e3b0dfc9cf89e5eb5f556a645064
sicentendu <ryancwilliams9@gmail.com>
2012-11-07 20:56:17 -0500

    gdsa

--- db/migrate/20120327201939_create_shows.rb
+++ db/migrate/20120327201939_create_shows.rb
@@ -1,11 +1,11 @@
 class CreateMicroposts < ActiveRecord::Migration
   def change
-    create_table :shows do |t|
+    create_table :microposts do |t|
       t.string :content
       t.integer :user_id
 
       t.timestamps
     end
-    add_index :shows, [:user_id, :created_at]
+    add_index :microposts, [:user_id, :created_at]
   end
 end
